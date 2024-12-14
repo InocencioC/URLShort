a0e4aac85652fafb80e623d3092e1b5ab7a60b67
Inocencio Cordeiro <cordeiroturorias@gmail.com>
2024-12-14 22:22:55 +0100

    first commit

---
+++ src/main/java/com/ino/urlshorter/UrLshorterApplication.java
@@ -0,0 +1,13 @@
+package com.ino.urlshorter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrLshorterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UrLshorterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ino/urlshorter/UrLshorterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ino.urlshorter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrLshorterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
